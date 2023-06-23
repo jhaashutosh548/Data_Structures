51f14aae4214ef4e3af85bb8e39bef15a9503b56
Purohitsushil <106049274+Purohitsushil@users.noreply.github.com>
2023-06-23 20:32:32 +0530

    Linklist part 2

--- linklist.py
+++ linklist.py
@@ -1,4 +1,5 @@
 print('------------------------------------------')
+# creating linklist
 class node:
     def __init__(self, data, next = None):
         self.data = data
@@ -8,6 +9,7 @@ class linklist:
     def __init__(self, head = None):
         self.head = head
     
+    # fuction to add data at the end of the list
     def add(self, data,):
         new = node(data)
         if (self.head):
@@ -18,22 +20,29 @@ class linklist:
         else:
             self.head = new
 
+    
+    # function display the data list
     def display(self, first=None):
         self.first = first
         first = self.head
+        print('------------')
         while(first):
-            print('data')
-            print(first.data)
-            print('address')
-            print(first.next)
+            # print('data')
+            print(first.data, end = ' ')
+            # print('address')
+            # print(first.next)
             first= first.next
+        print('------------')
 
+    # add data at the start of the linklist
     def insert(self,new = None):
         self.new = new
         newnode = node(new) 
         newnode.next = self.head
         self.head = newnode 
 
+
+    # add data at the middle of the linklist
     def insertmid(self, data, x):
         self.data = data
         self.x = x
@@ -44,6 +53,40 @@ class linklist:
                 newnode.next = temp.next
                 temp.next = newnode
             temp = temp.next
+    
+    # delete the data in the linklist
+    def delete(self,value):
+        temp = self.head
+        while(temp!= None):
+            if temp.data == value:
+                break
+            prev = temp
+            temp = temp.next
+        prev.next = temp.next
+
+    #search the data 
+    def search(self,x):
+        temp = self.head
+        while(temp!=None):
+            if (temp.data == x):
+                return True
+            temp = temp.next
+        return False
+    
+    #sorting the ll
+    def sort(self):
+        temp = self.head
+        a = []
+        while(temp):
+            a.append(temp.data)
+            temp = temp.next 
+        a.sort()
+        x = linklist()
+        for i in a:
+            x.add(i)
+        return x
+
+
 
 
 print('craeteinga ll')
@@ -57,8 +100,26 @@ print(item.head.data)
 print('add data in the start ')
 item.insert(10)
 item.display()
+
 item.insertmid(55,30)
 item.display()
+
+item.delete(55)
+item.display()
+
+print('searching 55 in ll ',item.search(40))
+
+item.add(5)
+item.add(34)
+item.display()
+
+item = item.sort()
+item.display()
+
+
+
+
+
 
 
 
