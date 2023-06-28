6d0886720a4dde9cb27737f20a0079d0db668f93
Purohitsushil <106049274+Purohitsushil@users.noreply.github.com>
2023-06-28 21:15:11 +0530

    sorting

--- Sort_algo.py
+++ Sort_algo.py
@@ -66,6 +66,64 @@ arr = [12,34,2,98,46,19,45,54,32,74,15,34]
 margesort(arr,0,len(arr)-1)
 print(arr)
 
+#tc = O(nlogn) recus
+#tc = O(n^2) normal
+#sc = O(n)
+
+# selection sort
+
+def sectionsort(x):
+    l = len(x)
+    for i in range(0,l):
+        min_ind = i
+        for j in range(i+1,l):
+            if x[j] < x[min_ind]:
+                min_ind = j
+        x[i],x[min_ind] = x[min_ind],x[i]
+
+arr = [12,34,2,98,46,19,45,54,32,74,15,34]
+sectionsort(arr)
+print('selection sort : ',arr)
+
+
+def bubblesort(x):
+    l = len(x)
+    swapped = False
+    for i in range(l-1):
+        for j in range(0,l-i-1):
+            if x[j]>x[j+1]:
+                swapped = True
+                x[j],x[j+1]=x[j+1],x[j]
+        if not swapped:
+            return
+        
+arr = [12,34,2,98,46,19,45,54,32,74,15,34]
+bubblesort(arr)
+print('bubble sort : ',arr)
+        
+
+def insertionsort(x):
+    l = len(x)
+    if l<=1:
+        return
+    for i in range(1,l):
+        n = x[i]
+        j = i-1
+        while j>=0 and n < x[j]:
+            x[j+1] = x[j]
+            j=j-1
+        x[j+1]=n
+
+arr = [12,34,2,98,46,19,45,54,32,74,15,34]
+insertionsort(arr)
+print('insertion sort : ',arr)
+
+
+
+
+
+
+
 
 
 
